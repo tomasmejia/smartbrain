52f3f43d88508141fc9c36487063c81bb5e05687
Cloudburst <tomas.mejia.ed@hotmail.com>
2018-04-18 22:26:53 -0300

    added logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import Tilt from 'react-tilt'
+import brain from './brain.png'
 import './Logo.css';
 
 const Logo = () => {
     return (
         <div className='ma4 mt0'>
             <Tilt className="Tilt br2 shadow-2 pa2" options={{ max: 55 }} style={{ height:  180, width: 180 }} >
-                <div className="Tilt-inner">
-                    <div className="ma2"><span role="img" aria-labelledby="panda1">👽</span></div>
-                    <p className="silver f3 courier b">I WANT TO BELIEVE</p>
-                     </div>
+                <div className="Tilt-inner pa3">
+                    <div className="ma2"><img src={brain} alt="Brain logo" srcset=""/></div>
+                </div>
             </Tilt>
         </div>
     )
