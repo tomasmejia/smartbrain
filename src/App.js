2404d35ae793d710d5babc9f94184d4edf0ffe5f
Cloudburst <tomas.mejia.ed@hotmail.com>
2018-05-03 21:40:30 -0300

    Connected the server to the front-end

--- src/App.js
+++ src/App.js
@@ -136,7 +136,7 @@ class App extends Component {
       isSignedIn: false
     }
   }
-
+  
   calculateFaceLocation = (data) => {
     // we get the bouding box data
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
